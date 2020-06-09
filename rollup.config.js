b7caddd49ec8a1e0b94efa3af2040b5541fde76c
Lars Hupel <lars.hupel@innoq.com>
2020-06-09 09:35:29 +0200

    update Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,20 @@
-import resolve from "@rollup/plugin-node-resolve";
 import sucrase from "@rollup/plugin-sucrase";
 
 export default {
     input: "src/index.ts",
-    output: {
-        file: "dist/index.es.js",
-        format: "esm"
-    },
+    output: [
+        {
+            file: "dist/index.es.js",
+            format: "esm"
+        },
+        {
+            file: "dist/index.js",
+            format: "cjs"
+        }
+    ],
     plugins: [
-        resolve({ extensions: [".js", ".ts"] }),
         sucrase({
-            exclude: [`${root}/node_modules/**`],
+            exclude: ["node_modules/**"],
             transforms: ["typescript"]
         })
     ]
